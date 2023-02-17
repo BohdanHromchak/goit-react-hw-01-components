03a8059152205a5bc5b44f5a71fdc639bd79ea22
BohdanHromchak <gromchakbogdan@gmail.com>
2023-02-17 22:56:13 +0200

    upd

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { GlobalStyle } from './GlobalStyle';
+import user from '../user.json'
+import data from '../data.json'
+import friends from '../friends.json'
+import transactions from '../transactions.json'
+
+export const App = () => {
+
+  return (
+    <div>
+
+
+      
+      <GlobalStyle />
+    </div>
+  );
+};
